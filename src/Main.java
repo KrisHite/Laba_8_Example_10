7cc9d2a6f70c2711d34b626116e28054b0b66b61
KrisHite <alexgusevmail@gmail.com>
2023-02-28 20:34:21 +0500

    Коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = null;
+        PrintWriter out = null;
+        try {
+// Создание потоков
+            br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\My\\MyFile1.txt"),"cp1251"));
+            out = new PrintWriter("C:\\My\\MyFile2.txt","cp1251");
+// Переписывание информации из одного файла в другой
+            int lineCount = 0;
+            String s;
+            while ((s = br.readLine()) != null) {
+                lineCount++;
+                out.println(lineCount + ": " + s);
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка !!!!!!!!"); }
+        finally{
+            br.close();
+            out.flush();
+            out.close();
+        }}
 }
